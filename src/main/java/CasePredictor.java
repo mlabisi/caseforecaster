7fe62745112443f98f59ba5c61d897bd398b1c25
Mora <mlabisi98@gmail.com>
2020-04-02 18:56:32 -0700

    spaghetti code is good to go, just need to find a model

--- src/main/java/CasePredictor.java
+++ src/main/java/CasePredictor.java
@@ -1,50 +1,57 @@
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.SequenceRecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVSequenceRecordReader;
 import org.datavec.api.records.writer.RecordWriter;
+import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.records.writer.impl.csv.CSVRecordWriter;
 import org.datavec.api.split.FileSplit;
-import org.datavec.api.split.InputSplit;
+import org.datavec.api.split.NumberedFileInputSplit;
 import org.datavec.api.split.partition.NumberOfRecordsPartitioner;
-import org.datavec.api.split.partition.Partitioner;
 import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.transform.condition.ConditionOp;
+import org.datavec.api.transform.condition.column.CategoricalColumnCondition;
+import org.datavec.api.transform.filter.ConditionFilter;
 import org.datavec.api.transform.schema.Schema;
 import org.datavec.api.writable.Writable;
-import org.datavec.local.transforms.LocalTransformExecutor;
 import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.stats.StatsListener;
 import org.deeplearning4j.ui.storage.FileStatsStorage;
-import org.joda.time.DateTimeZone;
-import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.evaluation.regression.RegressionEvaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
+import static org.joda.time.DateTimeZone.*;
+
 public class CasePredictor {
     private static Map<String, Integer> locationToFIPS = new HashMap<>();
+    private static Multimap<Integer, List<Writable>> trainToWrite = ArrayListMultimap.create();
+    private static Multimap<Integer, List<Writable>> testToWrite = ArrayListMultimap.create();
 
     private static MultiLayerNetwork cModel;
     private static File cModelFile;
@@ -57,38 +64,34 @@ public class CasePredictor {
     private static final String STATES_FILE = "covid-19-states.csv";
 
     public static final int seed = 12345;
-    private static int numEpochs = 15;
     public static final int batchSize = 100;
     public static final double learningRate = 0.01;
     public static final int numInputs = 2;
     private static final int numOutputs = 1;
+    private static final int labelIndex = 2;
+
 
     private static final String DATA_PATH = FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "itscoronatime/");
-    private static final Logger log = LoggerFactory.getLogger(CasePredictor.class);
     private static StatsStorage statsStorage = new FileStatsStorage(new File(System.getProperty("java.io.tmpdir"), "ui-stats.dl4j"));
+    private static File dir = new File(DATA_PATH);
+    private static File countiesDirTrain = new File(DATA_PATH + "counties-train/");
+    private static File countiesDirTest = new File(DATA_PATH + "counties-test/");
+    private static File statesDirTrain = new File(DATA_PATH + "states-train/");
+    private static File statesDirTest = new File(DATA_PATH + "states-test/");
 
 
     private static void runModel(String filename, String url) {
         boolean inCountyMode = filename.contains("counties");
         try {
-            File dir = new File(DATA_PATH);
             dir.mkdir();
+            countiesDirTrain.mkdir();
+            countiesDirTest.mkdir();
+            statesDirTrain.mkdir();
+            statesDirTest.mkdir();
 
             // store fresh copy of dataset in our cache
             File infile = new File(DATA_PATH + "src-" + filename);
             FileUtils.copyURLToFile(new URL(url), infile);
-
-            // create output files for the clean data
-            File trainOut = new File(DATA_PATH + filename);
-            File testOut = new File(DATA_PATH + filename);
-            if (trainOut.exists()) {
-                trainOut.delete();
-            }
-            if (testOut.exists()) {
-                testOut.delete();
-            }
-            trainOut.createNewFile();
-            testOut.createNewFile();
 
             // configure csv input schema
             Schema csvSchema = inCountyMode
@@ -104,26 +107,20 @@ public class CasePredictor {
             // clean the data
             TransformProcess tp = new TransformProcess.Builder(csvSchema)
                     .removeAllColumnsExceptFor("date", "fips", "cases")
-                    .stringToTimeTransform("date", "YYYY-MM-DD", DateTimeZone.UTC)
+                    .filter(new ConditionFilter(new CategoricalColumnCondition("fips", ConditionOp.InSet, new HashSet<>(Collections.singletonList("")))))
+                    .stringToTimeTransform("date", "YYYY-MM-DD", UTC)
                     .build();
 
+
             // grab the data from the original csv file
-            RecordReader recordReader = new CSVRecordReader(); // might need to use a sequential record reader
+            RecordReader recordReader = new CSVRecordReader(1); // might need to use a sequential record reader
             recordReader.initialize(new FileSplit(infile));
+            TransformProcessRecordReader processRecordReader = new TransformProcessRecordReader(recordReader, tp);
+            processRecordReader.initialize(new FileSplit(infile));
 
-            // prepare the output files for the clean data
-            RecordWriter trainingWriter = new CSVRecordWriter();
-            RecordWriter testingWriter = new CSVRecordWriter();
-            Partitioner pTrain = new NumberOfRecordsPartitioner();
-            Partitioner pTest = new NumberOfRecordsPartitioner();
-            trainingWriter.initialize(new FileSplit(trainOut), pTrain);
-            testingWriter.initialize(new FileSplit(testOut), pTest);
-
-            // process the original data
-            List<List<Writable>> rawTraining = new ArrayList<>();
-            List<List<Writable>> rawTesting = new ArrayList<>();
-            recordReader.next();
-
+            // process the original data and build location to fips map
+            // grab total number of rows to calculate training-testing split
+            int setSize = 0;
             while (recordReader.hasNext()) {
                 List<Writable> row = recordReader.next();
                 if (!(inCountyMode ? row.get(3) : row.get(2)).toString().equals("")) {
@@ -131,31 +128,39 @@ public class CasePredictor {
                     int fips = (inCountyMode ? row.get(3) : row.get(2)).toInt();
                     locationToFIPS.put(location, fips);
                 }
-                rawTraining.add(row);
+                setSize++;
+            }
+            recordReader.reset();
+
+            // prepare the output files for the clean data
+            RecordWriter trainingWriter = new CSVRecordWriter();
+            RecordWriter testingWriter = new CSVRecordWriter();
+            int ct = 0;
+
+            // process the clean data
+            while (processRecordReader.hasNext()) {
+                List<Writable> row = processRecordReader.next();
+                int fips = row.get(1).toInt();
+
+                // initialize file for clean data
+                if (ct++ < (0.7 * setSize)) {
+                    trainToWrite.put(fips, row);
+                } else {
+                    testToWrite.put(fips, row);
+                }
             }
 
-            for (int i = 0; i < (.07 * rawTraining.size()); i++) {
-                rawTesting.add(rawTraining.remove(i));
-            }
-
-            // export the clean data
-            List<List<Writable>> processedTrain = LocalTransformExecutor.execute(rawTraining, tp);
-            List<List<Writable>> processedTest = LocalTransformExecutor.execute(rawTesting, tp);
-            trainingWriter.writeBatch(processedTrain);
-            testingWriter.writeBatch(processedTest);
-            trainingWriter.close();
-            testingWriter.close();
-            System.out.println("Successfully wrote clean data to " + trainOut.getPath() + " and \n" + testOut.getPath());
+            collectRows(inCountyMode, trainingWriter, trainToWrite, countiesDirTrain, statesDirTrain);
+            collectRows(inCountyMode, testingWriter, testToWrite, countiesDirTest, statesDirTest);
 
             // save clean data as ml data sets
-            RecordReader trainingReader = new CSVRecordReader();
-            RecordReader testingReader = new CSVRecordReader();
-            trainingReader.initialize(new FileSplit(trainOut));
-            testingReader.initialize(new FileSplit(testOut));
+            SequenceRecordReader trainingReader = new CSVSequenceRecordReader(1);
+            trainingReader.initialize(new FileSplit(new File((inCountyMode ? countiesDirTrain.getPath() : statesDirTrain.getPath()) + "/")));
+            SequenceRecordReader testingReader = new CSVSequenceRecordReader(1);
+            trainingReader.initialize(new FileSplit(new File((inCountyMode ? countiesDirTest.getPath() : statesDirTest.getPath()) + "/")));
 
-            int labelIndex = 2;
-            DataSetIterator trainingData = new RecordReaderDataSetIterator(trainingReader, batchSize, labelIndex, labelIndex, true);
-            DataSetIterator testingData = new RecordReaderDataSetIterator(testingReader, batchSize, labelIndex, labelIndex, true);
+            DataSetIterator trainingData = new SequenceRecordReaderDataSetIterator(trainingReader, batchSize, numOutputs, labelIndex, true);
+            DataSetIterator testingData = new SequenceRecordReaderDataSetIterator(testingReader, batchSize, numOutputs, labelIndex, true);
 
             // configure the neural network
             final int numHiddenNodes = 50;
@@ -166,12 +171,12 @@ public class CasePredictor {
                     .list()
                     .layer(new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
                             .activation(Activation.TANH).build())
-//                    .layer(new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
-//                            .activation(Activation.TANH).build())
-                    .layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
-                            .activation(Activation.IDENTITY)
-                            .nIn(numHiddenNodes).nOut(numOutputs).build())
-                    .build();
+                    .layer(new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                            .activation(Activation.TANH).build())
+                    .layer(new RnnOutputLayer.Builder().name("output")
+                            .activation(Activation.TANH).nIn(numHiddenNodes).nOut(numOutputs)
+                            .lossFunction(LossFunctions.LossFunction.MSE).build())
+                            .build();
 
             // run the appropriate model
             if (inCountyMode) {
@@ -190,6 +195,20 @@ public class CasePredictor {
         }
     }
 
+    private static void collectRows(boolean inCountyMode, RecordWriter testingWriter, Multimap<Integer, List<Writable>> testToWrite, File countiesDirTest, File statesDirTest) {
+        testToWrite.asMap().forEach((fips, rows) -> {
+            try {
+                File out = new File((inCountyMode ? countiesDirTest.getPath() : statesDirTest.getPath()) + "/" + fips + ".csv");
+                out.createNewFile();
+                testingWriter.initialize(new FileSplit(out), new NumberOfRecordsPartitioner());
+                testingWriter.writeBatch(new ArrayList<>(rows));
+                testingWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
     private static void runModel(MultiLayerNetwork model, File modelFile, DataSetIterator testingData, DataSetIterator trainingData) {
         try {
             // initialize model
@@ -199,9 +218,8 @@ public class CasePredictor {
             model.setListeners(new StatsListener(statsStorage), new ScoreIterationListener(10));
 
             // train the model
-            for (int i = 0; i < numEpochs; i++) {
-                model.fit(trainingData);
-            }
+            int numEpochs = 15;
+            model.fit(trainingData, numEpochs);
 
             // save model to disk
             model.save(modelFile, true);
