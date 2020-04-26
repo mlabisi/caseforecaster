1ba41c3d438f3cebbb5ff06f60c9931260ec8abc
Mora <mlabisi98@gmail.com>
2020-04-26 03:17:46 -0700

    successfully generated sequences using spark

--- src/main/java/CasePredictor.java
+++ src/main/java/CasePredictor.java
@@ -1,7 +1,12 @@
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.SequenceRecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVSequenceRecordReader;
 import org.datavec.api.records.reader.impl.transform.TransformProcessRecordReader;
 import org.datavec.api.records.writer.RecordWriter;
 import org.datavec.api.records.writer.impl.csv.CSVRecordWriter;
@@ -12,9 +17,12 @@ import org.datavec.api.transform.condition.ConditionOp;
 import org.datavec.api.transform.condition.column.CategoricalColumnCondition;
 import org.datavec.api.transform.filter.ConditionFilter;
 import org.datavec.api.transform.schema.Schema;
-import org.datavec.api.transform.transform.time.DeriveColumnsFromTimeTransform;
+import org.datavec.api.transform.sequence.comparator.NumericalColumnComparator;
+import org.datavec.api.transform.transform.time.StringToTimeTransform;
 import org.datavec.api.writable.Writable;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.datavec.spark.transform.SparkTransformExecutor;
+import org.datavec.spark.transform.misc.StringToWritablesFunction;
+import org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.BackpropType;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -26,7 +34,6 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.util.ModelSerializer;
-import org.joda.time.DateTimeFieldType;
 import org.joda.time.DateTimeZone;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.dataset.DataSet;
@@ -41,7 +48,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CasePredictor {
     private static final String COUNTRIES_URL = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv";
@@ -158,32 +164,43 @@ public class CasePredictor {
             // configure csv input schema
             Schema csvSchema = inCountyMode
                     ? new Schema.Builder()
-                    .addColumnsString("date", "county", "state")
-                    .addColumnsInteger("fips", "cases", "deaths")
+                    .addColumnsString("date", "county", "state", "fips")
+                    .addColumnsInteger("cases", "deaths")
                     .build()
                     : new Schema.Builder()
-                    .addColumnsString("date", "state")
-                    .addColumnsInteger("fips", "cases", "deaths")
+                    .addColumnsString("date", "state", "fips")
+                    .addColumnsInteger("cases", "deaths")
                     .build();
 
-            // clean the data
+            // create the transformation to be applied on raw data
             TransformProcess tp = new TransformProcess.Builder(csvSchema)
                     .removeAllColumnsExceptFor("date", "fips", "cases")
-                    .stringToTimeTransform("date", "YYYY-MM-dd", DateTimeZone.UTC)
-                    .filter(new ConditionFilter(new CategoricalColumnCondition("fips", ConditionOp.NotEqual, fips)))
-                    .integerToCategorical("fips", locationToFIPS.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey)))
-                    .categoricalToInteger("fips")
-                    .transform(new DeriveColumnsFromTimeTransform.Builder("date").addIntegerDerivedColumn("DayOfYear", DateTimeFieldType.dayOfYear()).build())
-                    .removeColumns("date")
-                    .renameColumn("DayOfYear", "dateNum")
+                    .filter(new ConditionFilter(new CategoricalColumnCondition("fips", ConditionOp.Equal, "")))
+                    .transform(new StringToTimeTransform("date", "YYYY-MM-dd", DateTimeZone.UTC))
+                    .convertToSequence("fips", new NumericalColumnComparator("date", true))
                     .build();
+
+            // now read in the raw data to memory using spark
+            SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Case Predictor");
+            JavaSparkContext sc = new JavaSparkContext(conf);
+            JavaRDD<String> rawData = sc.textFile(infile.getPath()).filter(row -> !row.startsWith("date"));
+
+            RecordReader reader = new CSVRecordReader();
+            StringToWritablesFunction swFunc = new StringToWritablesFunction(reader);
+            JavaRDD<List<Writable>> parsed = (JavaRDD<List<Writable>>) rawData.map(swFunc);
+
+            // execute the transform
+            JavaRDD<List<List<Writable>>> processedData = SparkTransformExecutor.executeToSequence(parsed, tp);
+
+            // save the clean data
+            String cleanPath = FilenameUtils.concat(rscDir.getPath(), "clean_" + infile.getName().replace(".csv", ""));
+            processedData.saveAsTextFile(cleanPath);
+
+
             TransformProcessRecordReader processedRecordReader = new TransformProcessRecordReader(recordReader, tp);
             processedRecordReader.initialize(new FileSplit(infile));
 
-            // prepare the output files for the clean data
-            String cleanDirPath = FilenameUtils.concat(rscDir.getPath(), "clean_" + infile.getName());
-            cleanFile = new File(cleanDirPath);
-            cleanFile.createNewFile();
+
             RecordWriter writer = new CSVRecordWriter();
 
             // process the clean data
@@ -193,7 +210,7 @@ public class CasePredictor {
             }
 
             // write the clean data
-            writeCleanData(writer, toWrite, cleanDirPath);
+            writeCleanData(writer, toWrite, cleanPath);
             toWrite.clear();
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
@@ -203,56 +220,62 @@ public class CasePredictor {
     }
 
     private void writeCleanData(RecordWriter writer, List<List<Writable>> toWrite, String outPath) {
-            try {
-                File out = new File(outPath);
-                out.createNewFile();
-                writer.initialize(new FileSplit(out), new NumberOfRecordsPartitioner());
-                writer.writeBatch(toWrite);
-                writer.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            File out = new File(outPath);
+            out.createNewFile();
+            writer.initialize(new FileSplit(out), new NumberOfRecordsPartitioner());
+            writer.writeBatch(toWrite);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initiateTestTrain(File file) {
-            try {
-                // read in data
-                RecordReader reader = new CSVRecordReader();
-                reader.initialize(new FileSplit(file));
-                DataSetIterator iter = new RecordReaderDataSetIterator.Builder(reader, 100)
-                        .regression(2)
-                        .build();
-                DataSet data = iter.next();
+        try {
+            // read in data
+            SequenceRecordReader reader = new CSVSequenceRecordReader();
+            reader.initialize(new FileSplit(file));
+            DataSetIterator iter = new SequenceRecordReaderDataSetIterator(reader, 50, 1, 2, true);
+            DataSet data = iter.next();
 
-                // split data into training and testing sets
-                SplitTestAndTrain ttSplit = data.splitTestAndTrain(0.7);
-                DataSet trainingData = ttSplit.getTrain();
-                DataSet testingData = ttSplit.getTest();
+            // normalize training data, then set preprocessor
+            SplitTestAndTrain ttSplit = data.splitTestAndTrain(0.7);
+            DataSet trainingData = ttSplit.getTrain();
 
-                // build or grab the model
-                MultiLayerNetwork model = getModel();
+//                NormalizerMinMaxScaler normalizer = new NormalizerMinMaxScaler();
+//                normalizer.fit(trainingData);
+//                iter.setPreProcessor(normalizer);
+//
+//                // split data into training and testing sets
+//                ttSplit = data.splitTestAndTrain(0.7);
+//                trainingData = ttSplit.getTrain();
+            DataSet testingData = ttSplit.getTest();
 
-                // train model
-                for(int i = 0; i < 100000; i++) {
-                    model.fit(trainingData);
-                    model.rnnClearPreviousState();
-                }
+            // build or grab the model
+            MultiLayerNetwork model = getModel();
 
-                // save model
-                ModelSerializer.writeModel(model, FilenameUtils.concat(rscDir.getPath(), "model.zip"), true);
-
-                // test model
-                testModel(testingData, model);
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
+            // train model
+            for (int i = 0; i < 1000; i++) {
+                model.fit(trainingData);
+                model.rnnClearPreviousState();
             }
+
+            // save model
+            ModelSerializer.writeModel(model, FilenameUtils.concat(rscDir.getPath(), "model.zip"), true);
+
+            // test model
+            testModel(testingData, model);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private void testModel(DataSet testingData, MultiLayerNetwork model) {
         double[] predicted = new double[testingData.numExamples()];
         double[] actual = new double[testingData.numExamples()];
 
-        for(int i = 0; i < testingData.numExamples(); i++) {
+        for (int i = 0; i < testingData.numExamples(); i++) {
             predicted[i] = model.rnnTimeStep(testingData.get(i).getFeatures()).getDouble(0);
             actual[i] = testingData.get(i).getLabels().getDouble(0);
         }
